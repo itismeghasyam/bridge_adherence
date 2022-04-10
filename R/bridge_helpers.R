fa35d7d8bad6a8ab94249ef9a62359c1003d38d2
Ubuntu <ubuntu@ip-10-41-31-173.ec2.internal>
2022-04-10 16:42:32 +0000

    - explicitly stating pageSize in bridge Query

--- R/bridge_helpers.R
+++ R/bridge_helpers.R
@@ -5,7 +5,7 @@ bridgeclient_get_activityevents <- function(study_id,user_id){
 
 
 bridgeclient_get_studies <- function(){
-    bridgeclient:::bridgeGET("/v5/studies")
+    bridgeclient:::bridgeGET("v5/studies?offsetBy=0&pageSize=100&includeDeleted=false")
 }
 
 bridgeclient_get_adherence <- function(study_id,user_id){
