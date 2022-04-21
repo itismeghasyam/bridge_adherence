9fbdfd3157dbc0cefc0bb03007fecea8536eb9f8
Ubuntu <ubuntu@ip-10-41-29-110.ec2.internal>
2022-04-21 19:25:59 +0000

    handling offset of study number

--- R/bridge_helpers.R
+++ R/bridge_helpers.R
@@ -4,8 +4,8 @@ bridgeclient_get_activityevents <- function(study_id,user_id){
 }
 
 
-bridgeclient_get_studies <- function(){
-    bridgeclient:::bridgeGET("v5/studies?offsetBy=0&pageSize=100&includeDeleted=false")
+bridgeclient_get_studies <- function(offset){
+    bridgeclient:::bridgeGET(glue::glue("v5/studies?offsetBy={offset}&pageSize=100&includeDeleted=false"))
 }
 
 bridgeclient_get_adherence <- function(study_id,user_id){
